c3c6ea373f14c5ad46e701dd652e670f7534a01e
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-05-30 17:29:44 +0200

    Var test and t test for ex. 2

--- project.R
+++ project.R
@@ -12,6 +12,9 @@ data <- data[-c(k+1, j+1, i+1, j*k+1, i*j+1, i*k+1, i*j*k+1, i+j+k+1), ]
 # Ga ook op een formele manier na of de gegevens normaal verdeeld zijn.
 # Indien dit niet het geval is, in welke zin wijken de gegevens af van normaal verdeelde gegevens.
 # Kan je de gegevens transformeren naar normaal verdeelde gegevens? Bespreek.
+print("1. Variabele los")
+print("-------------------------------------------")
+
 los <- data$los
 print(paste0("IQR: ", IQR(los)))
 print(paste0("standaardeviatie: ", sd(los)))
@@ -21,6 +24,8 @@ max <- min_max[2]
 print(paste0("Min: ", min))
 print(paste0("Max: ", max))
 print(paste0("Range: ", max-min))
+shapiro.test(los)
+print(paste0("Shapiro: ", "W = 0.76851, p-value < 2.2e-16")) # Shapiro test om aan te tonen dat het niet normala verdeeld is, resultaat shapiro.test(los) er zelf ingeplakt, aangezien die functie de data returnt
 boxplot(los, ylab="Boxplot duur ziekenhuisbezoek (in dagen)") # Boxplot assymetrisch
 hist(los, ylab="Histogram duur ziekenhuisbezoek (in dagen)") # Assymetrisch # https://www.datamentor.io/r-programming/histogram/
 # https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/qqnorm
@@ -34,21 +39,42 @@ log_los <- log_los[is.finite(log_los)] # Verwijder de oneindige waarden (met one
 qqnorm(log_los, main="QQ-plot log van de duur ziekenhuisbezoek (in dagen)")
 boxplot(log_los, main="Boxplot log van de duur ziekenhuisbezoek (in dagen)")
 hist(log_los, main="Histogram log van de duur ziekenhuisbezoek (in dagen)")
-print(paste0("Approx: X~N(", mean(log_los), ", ", sd(log_los), ")"))
+avg <- mean(log_los)
+sdev <- sd(log_los)
+print(paste0("Approx: X~N(", avg, ", ", sdev, ")"))
+shapiro.test(log_los)
+print(paste0("Shapiro: ", "W = 0.97409, p-value = 1.197e-07")) # De log data is wel normaal verdeeld, # W staat voor r_Q # p-waarde zo laag waarschijnlijk door discrete dagen
+
 
 # 2. Hangt de duur van het ziekenhuisverblijf af van het levend of gestorven zijn van de
 # patiënten? M.a.w. is de duur van het ziekenhuisverblijf identiek verdeeld bij de popu-
 # latie levende en gestorven patiënten? Voer een gepaste test uit.
-# H_0: ze zijn afhankelijk van elkaar
-# H_1: Niet afhankelijk van elkaar
-# Schatter voor gemiddelde en sd ziekenhuisverblijf en zien of beide groepen binnen het aanvaardingsgebied vallen
+print("2. Verband duur, dead/alive")
+print("-------------------------------------------")
 
+# H_0: m = m
+# H_1: m != m
+# Schatter voor gemiddelde en sd ziekenhuisverblijf en zien of beide groepen binnen het aanvaardingsgebied vallen
+# Onduidelijk levend/gestorven na laatste opvolging of in het ziekenhuis, ik ga ervan uit dat het bij de laatste opvolging is
+alive <- data$los[data$fstat=="0"] # Patienten levend bij laatste opvolging
+dead <- data$los[data$fstat=="1"] # Patienten dood bij laatste opvolging
+var.test(alive, dead) # 0.6906 > 0.05 -> Var dus hetzelfde
+t.test(dead, alive, var.equal=TRUE) # 0.08266 > 0.05 -> H0 aanvaarden (zijn dus hetzelfde) -> Niet afhankelijk
+
+# p-value 0.08266 > 0.05 -> Reject, er is dus wel een verband tussen de twee
 
 # 3. Ga na of er een verband is tussen het type hartinfarct en de ontslagstatus uit het ziekenhuis
 # na opname. Voer opnieuw een gepaste test uit.
 # H_0: ze zijn afhankelijk van elkaar
 # H_1: Niet afhankelijk van elkaar
+print("3. Verband type hartinfarct/ontslagstatus")
+print("-------------------------------------------")
+# Discrete variabelen: X^2 test
+# Leuk tabelletje opstellen
 
 # 4. Kan je uit de leeftijd van de patiënt het BMI voorspellen? Beschrijf uitvoerig.
 # Verband tussen twee continue variabelen
+print("4. Leeftijd voorspellen uit BMI")
+print("-------------------------------------------")
+
 plot(data$age, data$bmi, type="p", xlab="Leeftijd", ylab="BMI", main="Verband leeftijd en BMI")
